ff0712168a4dd2e8b7bbae2bea03b529a0a81409
Hsa Moo Moo <hsamoomoo02@gmail.com>
2025-05-03 13:03:48 +0700

    Added index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings: "Think before you speak",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
